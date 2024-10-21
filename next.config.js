d9581baa8a72c369aaa667e03cec950a766e05ea
Michael Scheiwiller <mi.lappert@gmail.com>
2024-10-21 20:32:00 +0200

    fix(deployment): another try...

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/open-climate-fund" : "",
+  basePath: "",
 };
 
 export default nextConfig;
