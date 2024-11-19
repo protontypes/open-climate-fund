a909fc0488788a66db2a053485bbfed5d015acc9
Michael Scheiwiller <mi.lappert@gmail.com>
2024-11-19 14:24:08 +0100

    refactor: add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="OCF" />
+        <link rel="manifest" href="site.webmanifest" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
