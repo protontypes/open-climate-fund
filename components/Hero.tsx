ebf77b15fa30498d0dbf5319306f659760d15422
Michael Scheiwiller <mi.lappert@gmail.com>
2024-12-11 17:09:35 +0100

    refactor: update subtitle wording in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,12 +18,11 @@ export default function Hero() {
               className="text-5xl md:text-6xl lg:text-7xl font-semibold 
               tracking-tight text-white-500 leading-tight"
             >
-              Fund Open Source in<br className="hidden sm:inline" />
+              Fund Open Source in{" "}<br className="hidden sm:inline" />
               <span className="text-green-500">Climate & Sustainability</span>
             </h1>
             <p className="mt-6 text-xl text-silver-400 max-w-3xl">
-              Contribute to OpenSustain.tech and support open source projects
-              making a difference in our world.
+              Add projects to OpenSustain.tech to support open source making a difference in our world.
             </p>
           </div>
         </div>
